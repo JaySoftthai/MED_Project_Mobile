24177b624673c90f0ffd44e830535a63bfafeb0b
rowenhaward <adisak.m@softthai.com>
2018-07-28 17:12:23 +0700

    2018.07.28.001-1: Create Project

--- src/providers/omm-meeting-list/omm-meeting-list.ts
+++ src/providers/omm-meeting-list/omm-meeting-list.ts
@@ -2,10 +2,10 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';// import { Http } from '@angular/http';
 
 import { Observable } from 'rxjs/observable';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/observable/throw';
-import 'rxjs/add/operator/toPromise';
+// import 'rxjs/add/operator/map';
+// import 'rxjs/add/operator/catch';
+// import 'rxjs/add/observable/throw';
+// import 'rxjs/add/operator/toPromise';
 //import provider
 import { ApiProvider } from '../api/api';
 //import models
