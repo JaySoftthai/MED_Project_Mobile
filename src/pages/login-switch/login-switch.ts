6a44839042e333cb8788ce0bb78ddae86730b020
rowenhaward <adisak.m@softthai.com>
2018-07-28 13:20:33 +0700

    Create project#1

--- src/pages/login-switch/login-switch.ts
+++ src/pages/login-switch/login-switch.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, ToastController, LoadingController, AlertController } from 'ionic-angular';
+import { Platform } from 'ionic-angular';
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
 
-/**
- * Generated class for the LoginSwitchPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+///providers
+import { UserloginProvider } from '../../providers/userlogin/userlogin';
+
+//models
+import { UserAccount } from '../../models/UserAccount';
+
+///Pages
+import { LockscreenPage } from '../lockscreen/lockscreen';
 
 @Component({
   selector: 'page-login-switch',
@@ -14,11 +18,52 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class LoginSwitchPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  sUsername: string;
+  sPassword: string;
+  sPIN: string;
+  sPIN1: string;
+  sPIN2: string;
+  sPIN3: string;
+  sPIN4: string;
+  sPIN5: string;
+  sPIN6: string;
+  constructor(public navCtrl: NavController, public navParams: NavParams
+    , public toast: ToastController
+    , public popup: AlertController
+    , public loading: LoadingController
+    , public usrProvider: UserloginProvider
+    , public platform: Platform
+    , public sqlite: SQLite
+    , public storage: Storage
+  ) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginSwitchPage');
+
+
+    this.usrProvider.login(this.sUsername, this.sPassword).then((res: UserAccount) => {
+    });
+
   }
 
+
+
+
+
+
+  //show toast
+  presentToast(sMsg) {
+    let toast = this.toast.create({
+      message: sMsg,
+      duration: 3000,
+      position: 'buttom'
+    });
+
+    toast.onDidDismiss(() => {
+      //console.log('Dismissed toast');
+    });
+
+    toast.present();
+  }
 }
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -3,15 +3,16 @@ import { IonicPage, NavController, NavParams, ToastController, LoadingController
 import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 import { Storage } from '@ionic/storage';
 import { Platform } from 'ionic-angular';
-import { SQLite ,SQLiteObject} from '@ionic-native/sqlite';
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
 
 //import page
-import { LockscreenPage } from'../lockscreen/lockscreen';
+import { LoginSwitchPage } from '../login-switch/login-switch';
+import { LockscreenPage } from '../lockscreen/lockscreen';
 import { SetpinPage } from '../setpin/setpin';
 //import provider
-import{ UserloginProvider } from '../../providers/userlogin/userlogin';
+import { UserloginProvider } from '../../providers/userlogin/userlogin';
 //models
-import{ UserAccount } from '../../models/UserAccount';
+import { UserAccount } from '../../models/UserAccount';
 import { HomePage } from '../home/home';
 @Component({
   selector: 'page-login',
@@ -19,34 +20,34 @@ import { HomePage } from '../home/home';
 })
 export class LoginPage {
 
-  sUsername:string;
-  sUsername1:string;
-  sPassword:string;
+  sUsername: string;
+  sUsername1: string;
+  sPassword: string;
   myForm: FormGroup;
   txtUsername: FormControl;
   txtPassword: FormControl;
-  UserData :UserAccount;
-  isLogin:boolean;
+  UserData: UserAccount;
+  isLogin: boolean;
   constructor(public navCtrl: NavController, public navParams: NavParams
     , private formBdr: FormBuilder
-    ,public toast :ToastController
-    ,public popup:AlertController
-    ,public loading:LoadingController
-    ,public usrProvider:UserloginProvider
-    ,public platform:Platform
-    ,public sqlite :SQLite
-    ,public storage:Storage) {
-       //ตรวจสอบความถูกต้องของฟอร์ม เช่น required, minLength
+    , public toast: ToastController
+    , public popup: AlertController
+    , public loading: LoadingController
+    , public usrProvider: UserloginProvider
+    , public platform: Platform
+    , public sqlite: SQLite
+    , public storage: Storage) {
+    //ตรวจสอบความถูกต้องของฟอร์ม เช่น required, minLength
     this.txtUsername = this.formBdr.control('', Validators.required);
     this.txtPassword = this.formBdr.control('', Validators.compose([Validators.required, Validators.minLength(8)]));
-    this.myForm = this.formBdr.group({ 'txtUsername': this.txtUsername, 'txtPassword': this.txtPassword});
+    this.myForm = this.formBdr.group({ 'txtUsername': this.txtUsername, 'txtPassword': this.txtPassword });
     this.usrProvider.CreateTableLogin();
 
-    this.usrProvider.getUserNameFromStorage().then((res:string)=>{
-     this.sUsername1 = res;
+    this.usrProvider.getUserNameFromStorage().then((res: string) => {
+      this.sUsername1 = res;
     });
-    console.log( this.storage.ready());
-   // this.storage.ready().then(() => { this.storage.set('username', "1234"); });
+    console.log(this.storage.ready());
+    // this.storage.ready().then(() => { this.storage.set('username', "1234"); });
     this.storage.get('username').then((name) => {
       console.log('Me: Hey, ' + name + '! You have a very nice name.');
     });
@@ -62,53 +63,48 @@ export class LoginPage {
       this.isLogin = res;
       console.log('isLogin:' + this.isLogin);
       if (this.isLogin) {
-        this.navCtrl.setRoot(LockscreenPage);
+        this.navCtrl.setRoot(LoginSwitchPage);//this.navCtrl.setRoot(LockscreenPage);
         return false;
       }
-       else { return false; }
+      else { return false; }
     });
   }
-  login(){
+  login() {
     //รับข้อมูลจากฟอร์ม
     let sUsername = this.myForm.controls['txtUsername'].value;
     let sPassword = this.myForm.controls['txtPassword'].value;
-    console.log("sUsername "+sUsername);
-    console.log("sPassword "+sPassword);
-    if(sUsername != "" &&sPassword!="" )
-    {
-      this.usrProvider.login(sUsername,sPassword).then((res: UserAccount) => {
+    console.log("sUsername " + sUsername);
+    console.log("sPassword " + sPassword);
+    if (sUsername != "" && sPassword != "") {
+      this.usrProvider.login(sUsername, sPassword).then((res: UserAccount) => {
         this.UserData = res;
-        if (res != undefined && res.sResult != null) 
-        {
-            console.log("res "+ res.sFirstName);
-            if(res.sResult=="Success")
-            {
-              console.log(this.UserData);
-              this.navCtrl.push(SetpinPage,{UserAccountData:this.UserData},{animate:false});
-            }
-            else
-            {
-              //แจ้งเตือนกรณี login not success
-              let alert = this.popup.create({ title: res.sMsg1, buttons: ['ตกลง'] });
-              alert.present();
-            }
+        if (res != undefined && res.sResult != null) {
+          console.log("res " + res.sFirstName);
+          if (res.sResult == "Success") {
+            console.log(this.UserData);
+            this.navCtrl.push(SetpinPage, { UserAccountData: this.UserData }, { animate: false });
+          }
+          else {
+            //แจ้งเตือนกรณี login not success
+            let alert = this.popup.create({ title: res.sMsg1, buttons: ['ตกลง'] });
+            alert.present();
+          }
         }
-        else{ 
-          this.presentToast("No response."); 
+        else {
+          this.presentToast("No response.");
         }
       }).catch(error => {
         this.presentToast("Login error.");
       });
     }
-    else
-    {
+    else {
       //แจ้งเตือนกรณีไม่กรอก username / password
       let alert = this.popup.create({ title: "Please enter Username and Password", buttons: ['ตกลง'] });
       alert.present();
     }
-}
-  register(){
-       this.navCtrl.push(LockscreenPage, {}, {animate: false});
+  }
+  register() {
+    this.navCtrl.push(LockscreenPage, {}, { animate: false });
   }
   //show toast
   presentToast(sMsg) {
